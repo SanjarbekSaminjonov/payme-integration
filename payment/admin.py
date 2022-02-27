c044c7fc8887eeb0c175bdf213f2d7b7f4f3a284
Sanjarbek <sanjarbeksaminjonovv@gmail.com>
2022-02-27 15:42:51 +0500

    Update admin.py

--- payment/admin.py
+++ payment/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
 
-admin.site.site_header = 'World Glamour Tour Payment Administration'
+admin.site.site_header = 'Payment Administration'
 
 admin.site.unregister(Group)
 admin.site.unregister(User)
