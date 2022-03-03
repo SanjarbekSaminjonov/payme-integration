3b78242b4ffd0dfeaf3b65855c505a00c7713385
Sanjarbek <sanjarbeksaminjonovv@gmail.com>
2022-03-03 18:07:45 +0500

    Update views.py

--- payment/views.py
+++ payment/views.py
@@ -5,7 +5,8 @@ from order.models import Order
 
 class CheckOrder(Paycom):
     def check_order(self, amount, account, **kwargs):
-        order = Order.objects.filter(id=account['order_id']).first()
+        order_id = int(account['order_id'])
+        order = Order.objects.filter(id=order_id).first()
         if order is not None:
             if str(amount) != order.amount:
                 return self.INVALID_AMOUNT
@@ -14,12 +15,14 @@ class CheckOrder(Paycom):
             return self.ORDER_NOT_FOND
 
     def successfully_payment(self, account, transaction, *args, **kwargs):
-        order = Order.objects.filter(id=account['order_id']).first()
+        order_id = int(account['order_id'])
+        order = Order.objects.filter(id=order_id).first()
         order.is_payed = True
         order.save()
 
     def cancel_payment(self, account, transaction, *args, **kwargs):
-        order = Order.objects.filter(id=account['order_id']).first()
+        order_id = int(account['order_id'])
+        order = Order.objects.filter(id=order_id).first()
         order.is_payed = False
         order.save()
 
