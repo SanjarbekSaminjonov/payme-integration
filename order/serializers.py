98c43ba820f17b12545894372fd7c0a32136ae75
Sanjarbek <sanjarbeksaminjonovv@gmail.com>
2022-03-04 21:54:59 +0500

    update serializers

--- order/serializers.py
+++ order/serializers.py
@@ -2,26 +2,7 @@ from rest_framework.serializers import ModelSerializer
 from .models import Order
 
 
-class OrderCreateSerializer(ModelSerializer):
+class OrderSerializer(ModelSerializer):
     class Meta:
         model = Order
-        fields = (
-            'amount',
-            'number_of_people',
-            'place_id',
-            'place_name',
-            'customer_full_name',
-            'customer_passport',
-            'customer_phone_number',
-        )
-
-
-class OrderViewSerializer(ModelSerializer):
-    class Meta:
-        model = Order
-        fields = (
-            'is_payed',
-            'amount',
-            'place_id',
-            'customer_full_name',
-        )
+        fields = '__all__'
--- order/views.py
+++ order/views.py
@@ -1,15 +1,15 @@
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from .models import Order
-from .serializers import OrderCreateSerializer, OrderViewSerializer
+from .serializers import OrderSerializer
 
 # Create your views here.
 
 
 class OrderCreateAPIView(CreateAPIView):
     queryset = Order.objects.all()
-    serializer_class = OrderCreateSerializer
+    serializer_class = OrderSerializer
 
 
 class OrderDetailAPIView(RetrieveAPIView):
     queryset = Order.objects.all()
-    serializer_class = OrderViewSerializer
+    serializer_class = OrderSerializer
